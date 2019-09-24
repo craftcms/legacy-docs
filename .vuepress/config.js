436599d380d366ee70d86885cbefb44584e42331
Brad Bell <brad@pixelandtonic.com>
2019-09-24 13:34:16 -0700

    v2

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,34 +1,21 @@
 module.exports = {
+    title: 'Craft 2 Documentation',
+    description: 'Craft 2 Documentation',
     theme: 'craftdocs',
-    ga: 'UA-39036834-9',
-    base: '/v3/',
+    base: '/v2/',
     shouldPrefetch: () => false,
-    locales: {
-        '/': {
-            lang: 'en-US',
-            title: 'Craft 3 Documentation',
-        },
-        '/ja/': {
-            lang: 'ja',
-            title: 'Craft 3 ドキュメント',
-        }
-    },
+    ga: 'UA-39036834-8',
     themeConfig: {
         docsRepo: 'craftcms/cms',
         docsDir: 'docs',
-        docsBranch: 'develop',
+        docsBranch: 'develop-v2',
         editLinks: true,
-        searchMaxSuggestions: 10,
-        locales: {
-            '/': require('./config-en'),
-            '/ja/': require('./config-ja')
-        },
         nav: [
             {
                 text: 'Craft CMS',
                 items: [
-                    { text: 'Craft 2 Documentation', link: 'https://docs.craftcms.com/v2/' },
-                    { text: 'Craft 3 Documentation', link: '/' },
+                    { text: 'Craft 2 Documentation', link: '/' },
+                    { text: 'Craft 3 Documentation', link: 'https://docs.craftcms.com/v3/' },
                     { text: 'Craft 2 Class Reference', link: 'https://docs.craftcms.com/api/v2/' },
                     { text: 'Craft 3 Class Reference', link: 'https://docs.craftcms.com/api/v3/' },
                 ]
@@ -43,9 +30,229 @@ module.exports = {
             },
             { text: 'Craftnet API', link: 'https://docs.api.craftcms.com/' },
         ],
+        sidebar: {
+            '/plugins/': [
+                {
+                    title: 'Plugin Development',
+                    collapsable: false,
+                    children: [
+                        'introduction',
+                        'setting-things-up',
+                        'plugin-settings',
+                        'templates',
+                        'resources',
+                        'database',
+                        'internationalization',
+                        'hooks-and-events'
+                    ]
+                },
+                {
+                    title: 'Components',
+                    collapsable: false,
+                    children: [
+                        'controllers',
+                        'element-actions',
+                        'widgets',
+                        'field-types',
+                        'models',
+                        'records',
+                        'services',
+                        'variables'
+                    ]
+                },
+                {
+                    title: 'References',
+                    collapsable: false,
+                    children: [
+                        'hooks-reference',
+                        'events-reference'
+                    ]
+                },
+                {
+                    title: 'Guides',
+                    collapsable: false,
+                    children: [
+                        'migrations',
+                        'working-with-elements'
+                    ]
+                }
+            ],
+            '/templating/': [
+                ['../templating-overview', '← Templating Overview'],
+                {
+                    title: 'Templating Reference',
+                    collapsable: false,
+                    children: [
+                        'filters',
+                        'functions',
+                        'global-variables'
+                    ]
+                },
+                {
+                    title: 'Tags',
+                    collapsable: false,
+                    children: [
+                        'cache',
+                        'exit',
+                        'header',
+                        'includecss',
+                        'includecssfile',
+                        'includehirescss',
+                        'includejs',
+                        'includejsfile',
+                        'nav',
+                        'paginate',
+                        'redirect',
+                        'requirelogin',
+                        'requirepermission',
+                        'switch'
+                    ]
+                },
+                {
+                    title: 'Querying Elements',
+                    collapsable: false,
+                    children: [
+                        'craft.assets',
+                        'craft.categories',
+                        'craft.entries',
+                        'craft.tags',
+                        'craft.users',
+                        'eager-loading-elements'
+                    ]
+                },
+                {
+                    title: 'Variables',
+                    collapsable: false,
+                    children: [
+                        'assetfilemodel',
+                        'assetfoldermodel',
+                        'assetsourcemodel',
+                        'categorygroupmodel',
+                        'categorymodel',
+                        'datetime',
+                        'elementcriteriamodel',
+                        'entrymodel',
+                        'entrytypemodel',
+                        'globalsetmodel',
+                        'matrixblockmodel',
+                        'sectionmodel',
+                        'taggroupmodel',
+                        'tagmodel',
+                        'usergroupmodel',
+                        'usermodel'
+                    ]
+                },
+                {
+                    title: 'More',
+                    collapsable: false,
+                    children: [
+                        'craft.config',
+                        'craft.feeds',
+                        'craft.fields',
+                        'craft.globals',
+                        'craft.request',
+                        'craft.sections',
+                        'craft.session'
+                    ]
+                },
+                {
+                    title: 'Examples',
+                    collapsable: false,
+                    children: [
+                        'integrating-disqus',
+                        'rss-feed',
+                        'atom-feed',
+                        'entry-form',
+                        'search-form',
+                        'login-form',
+                        'user-profile-form',
+                        'user-registration-form',
+                        'forgot-password-form',
+                        'set-password-form',
+                    ]
+                }
+            ],
+            '/': [
+                {
+                    title: 'Introduction',
+                    collapsable: false,
+                    children: [
+                        '',
+                        'code-of-conduct'
+                    ]
+                },
+                {
+                    title: 'Installing & Updating',
+                    collapsable: false,
+                    children: [
+                        'requirements',
+                        'installing',
+                        'updating'
+                    ]
+                },
+                {
+                    title: 'Getting Started',
+                    collapsable: false,
+                    children: [
+                        'folder-structure'
+                    ]
+                },
+                {
+                    title: 'Configuration',
+                    collapsable: false,
+                    children: [
+                        'config-settings',
+                        'multi-environment-configs',
+                        'php-constants'
+                    ]
+                },
+                {
+                    title: 'Core Concepts',
+                    collapsable: false,
+                    children: [
+                        'sections-and-entries',
+                        'fields',
+                        'categories',
+                        'assets',
+                        'users',
+                        'globals',
+                        'tags',
+                        'relations',
+                        'routing',
+                        'searching',
+                        'reference-tags',
+                        ['localization-guide', 'Localization'],
+                        'static-translations',
+                    ]
+                },
+                {
+                    title: 'Templating',
+                    collapsable: false,
+                    children: [
+                        'templating-overview',
+                        'twig-primer',
+                        ['templating/filters', 'Templating Reference']
+                    ]
+                },
+                {
+                    title: 'Plugin Development',
+                    collapsable: false,
+                    children: [
+                        ['plugins/introduction', 'Plugin Development']
+                    ]
+                }
+            ]
+        },
         codeLanguages: {
             twig: 'Twig',
             php: 'PHP',
+        },
+        algolia: {
+            apiKey: '1014b55e7f916b20c5d6834bf7666dc3',
+            indexName: 'craftcms',
+            algoliaOptions: {
+                facetFilters: ['version:v2', 'tags:doc']
+            }
         }
     },
     markdown: {
@@ -60,12 +267,11 @@ module.exports = {
         config(md) {
             md
                 .use(replaceApiLinks)
-                .use(require('vuepress-theme-craftdocs/markup'))
                 .use(require('markdown-it-deflist'))
-                .use(require('markdown-it-imsize'))
+                .use(require('vuepress-theme-craftdocs/markup'))
         }
-    },
-}
+    }
+};
 
 function replaceApiLinks(md) {
     // code adapted from the markdown-it-replace-link plugin
@@ -101,29 +307,39 @@ function replaceApiLinks(md) {
 
 function replaceApiLink(link) {
     link = decodeURIComponent(link)
-    let m = link.match(/^(?:api:)?\\?([\w\\]+)(?:::\$?(\w+)(\(\))?)?(?:#([\w\-]+))?$/)
+    let m = link.match(/^api:\\?([\w\\]+)(?:::\$?(\w+)(\(\))?)?(?:#([\w\-]+))?$/)
     if (m) {
         let className = m[1]
         let subject = m[2]
         let isMethod = typeof m[3] !== 'undefined'
         let hash = m[4]
 
-        if (className.match(/^craft\\/) || className.match(/^Craft/)) {
-            let url = 'https://docs.craftcms.com/api/v3/'+className.replace(/\\/g, '-').toLowerCase()+'.html'
+        if (className.match(/^Craft\\/)) {
+            let url = 'https://docs.craftcms.com/api/v2/'+className.replace(/\\/g, '-').toLowerCase()+'.html'
             if (subject) {
                 hash = ''
                 if (isMethod) {
                     hash = 'method-'
+                } else if (!subject.match(/^EVENT_/)) {
+                    hash = 'property-'
                 }
                 hash += subject.replace(/_/g, '-').toLowerCase()
             }
             return url + (hash ? `#${hash}` : '');
         }
 
-        if (className.match(/^yii\\/) || className.match(/^Yii/)) {
-            let url = 'https://www.yiiframework.com/doc/api/2.0/'+className.replace(/\\/g, '-').toLowerCase()
+        if (className.match(/^[CI][A-Z]/)) {
+            let url = 'https://www.yiiframework.com/doc/api/1.1/'+className.replace(/\\/g, '-').toLowerCase()
             if (subject) {
-                hash = (isMethod ? `${subject}()` : `\$${subject}`)+'-detail'
+                hash = subject+(isMethod ? '()' : '')+'-detail'
+            }
+            return url + (hash ? `#${hash}` : '');
+        }
+
+        if (className.match(/^Twig/)) {
+            let url = 'https://twig.symfony.com/api/1.x/'+className.replace(/\\/g, '/')+'.html'
+            if (subject) {
+                hash = '#method_'+subject
             }
             return url + (hash ? `#${hash}` : '');
         }
@@ -131,6 +347,6 @@ function replaceApiLink(link) {
 
     m = link.match(/^config:(.+)/)
     if (m) {
-        return '/config/config-settings.md#'+m[1].toLowerCase()
+        return '/config-settings.md#'+m[1].toLowerCase()
     }
 }
