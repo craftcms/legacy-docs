882f7d69f0e3c5eb62ecbad11d95543a6ab603b6
Matt Stein <matt@pixelandtonic.com>
2020-04-27 14:35:26 -0500

    Fix icon reference.

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -18,7 +18,7 @@ module.exports = {
   base: "/v3/",
   shouldPrefetch: () => false,
   head: [
-    ['link', { rel: 'icon', href: '/logo.png' }],
+    ['link', {rel: 'icon', href: 'https://docs.craftcms.com/siteicons/favicon-16x16.png'}],
     ['link', {rel: 'apple-touch-icon', sizes: '180x180', href: 'https://docs.craftcms.com/siteicons/apple-touch-icon.png'}],
     ['link', {rel: 'icon', type: 'image/png', sizes: '32x32', href: 'https://docs.craftcms.com/siteicons/favicon-32x32.png'}],
     ['link', {rel: 'icon', type: 'image/png', sizes: '16x16', href: 'https://docs.craftcms.com/siteicons/favicon-16x16.png'}],
